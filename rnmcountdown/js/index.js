f8185b9ed7e7fbe56e17001c28aade3bcf6b0be8
EpicDJL <awesomeriptide@gmail.com>
2017-07-10 12:00:07 -0400

    Update index.js

--- rnmcountdown/js/index.js
+++ rnmcountdown/js/index.js
@@ -37,22 +37,40 @@ function initializeClock(id, endtime) {
   var timeinterval = setInterval(updateClock, 1000);
 }
 
-var ep2 = new Date("2017-07-30");
-var ep3 = new Date("2017-08-06");
-var ep4 = new Date("2017-08-13");
-var ep5 = new Date("2017-08-20");
-var ep6 = new Date("2017-08-27");
-var ep7 = new Date("2017-09-03");
-var ep8 = new Date("2017-09-10");
-var ep9 = new Date("2017-09-17");
-var ep10 = new Date("2017-09-23");
-function upDate() {
- if Date() < ep2
-  then var deadline = ep2
- if ep2 =< Date() < ep3
-  then var deadline = ep2
+
+
+var d = new Date()
+var ep2 = "2017-07-30"
+var ep3 = "2017-08-06"
+var ep4 = "2017-08-13"
+var ep5 = "2017-08-20"
+var ep6 = "2017-08-27"
+var ep7 = "2017-09-03"
+var ep8 = "2017-09-10"
+var ep9 = "2017-09-17"
+var ep10 = "2017-09-23"
+function updateTitle() {
+      if (d < new Date(ep2)) {window.deadline = ep2
+      document.getElementById("rnmep").innerHTML = "Rick and Morty S3E2";
+    } else if (new Date(ep2) < d && d < new Date(ep3)) {window.deadline = ep3
+      document.getElementById("rnmep").innerHTML = "Rick and Morty S3E3";
+    } else if (new Date(ep3) < d && d < new Date(ep4)) {window.deadline = ep4
+      document.getElementById("rnmep").innerHTML = "Rick and Morty S3E4";
+    } else if (new Date(ep4) < d && d < new Date(ep5)) {window.deadline = ep5
+      document.getElementById("rnmep").innerHTML = "Rick and Morty S3E5";
+    } else if (new Date(ep5) < d && d < new Date(ep6)) {window.deadline = ep6
+      document.getElementById("rnmep").innerHTML = "Rick and Morty S3E6";
+    } else if (new Date(ep6) < d && d < new Date(ep7)) {window.deadline = ep7
+      document.getElementById("rnmep").innerHTML = "Rick and Morty S3E7";
+    } else if (new Date(ep7) < d && d < new Date(ep8)) {window.deadline = ep8
+      document.getElementById("rnmep").innerHTML = "Rick and Morty S3E8";
+    } else if (new Date(ep8) < d && d < new Date(ep9)) {window.deadline = ep9
+      document.getElementById("rnmep").innerHTML = "Rick and Morty S3E9";
+    } else if (new Date(ep9) < d && d < new Date(ep10)) {window.deadline = ep10
+      document.getElementById("rnmep").innerHTML = "Rick and Morty S3E10";
+    }
+
 }
-console.log(ep2)
-console.log(ep3)
-console.log(ep4)
-initializeClock('clockdiv', deadline);
+updateTitle()
+
+  initializeClock('clockdiv', deadline);
